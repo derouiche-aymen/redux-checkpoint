5fcbedd7913bcc106c31cba9e91cd976ceb8bd31
derouiche-aymen <aymenonea@live.fr>
2021-01-04 15:31:02 +0100

    modify

--- tasks/src/index.js
+++ tasks/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux'
+import { createStore } from 'redux'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
 
+const store = createStore
 ReactDOM.render(
  <Provider store={store}>
     <App />
